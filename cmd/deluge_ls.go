a8bc766972d84ff307ce97b77ef823b871f7376f
kenstir <kenstir@gmail.com>
2025-03-11 09:51:46 -0400

    Factor out giant Run func

--- cmd/deluge_ls.go
+++ cmd/deluge_ls.go
@@ -49,86 +49,88 @@ var delugeValidColumns = []string{
 var delugeListCmd = &cobra.Command{
 	Use:   "ls",
 	Short: "List torrents",
-	Run: func(cmd *cobra.Command, args []string) {
-		// get and check the flags
-		verbosity := viper.GetInt("verbose")
-		filter := viper.GetString("deluge.filter")
-		noheader := viper.GetBool("deluge.noheader")
-		columns := viper.GetStringSlice("deluge.columns")
+	Run:   delugeListCmdRun,
+}
+
+func delugeListCmdRun(cmd *cobra.Command, args []string) {
+	// get and check the flags
+	verbosity := viper.GetInt("verbose")
+	filter := viper.GetString("deluge.filter")
+	noheader := viper.GetBool("deluge.noheader")
+	columns := viper.GetStringSlice("deluge.columns")
+	for _, column := range columns {
+		if !slices.Contains(delugeValidColumns, column) {
+			fmt.Printf("Unknown column: %s\n", column)
+			fmt.Printf("Valid values for --column: %s\n", strings.Join(delugeValidColumns, ", "))
+			os.Exit(1)
+		}
+	}
+
+	// create a deluge client
+	if verbosity > 0 {
+		fmt.Printf("server: %s\n", viper.GetString("deluge.server"))
+		fmt.Printf("port: %d\n", viper.GetUint("deluge.port"))
+		fmt.Printf("username: %s\n", viper.GetString("deluge.username"))
+		fmt.Printf("password: %s\n", viper.GetString("deluge.password"))
+	}
+	client := deluge.NewV2(deluge.Settings{
+		Hostname:             viper.GetString("deluge.server"),
+		Port:                 viper.GetUint("deluge.port"),
+		Login:                viper.GetString("deluge.username"),
+		Password:             viper.GetString("deluge.password"),
+		DebugServerResponses: true,
+	})
+
+	// connect
+	err := client.Connect(context.Background())
+	if err != nil {
+		fmt.Printf("Error connecting to deluge: %s\n", err)
+		os.Exit(1)
+	}
+	if verbosity > 0 {
+		fmt.Printf("Connected to deluge\n")
+	}
+
+	// get torrents
+	torrentsStatus, err := client.TorrentsStatus(context.Background(), deluge.StateUnspecified, nil)
+	if err != nil {
+		fmt.Printf("Error getting torrents status: %s\n", err)
+		os.Exit(1)
+	}
+	if verbosity > 0 {
+		fmt.Printf("Found %d torrents\n", len(torrentsStatus))
+	}
+
+	// sort torrentsStatus by name
+	keys := make([]string, 0, len(torrentsStatus))
+	for key, _ := range torrentsStatus {
+		keys = append(keys, key)
+	}
+	sort.Slice(keys, func(i, j int) bool {
+		return torrentsStatus[keys[i]].Name < torrentsStatus[keys[j]].Name
+	})
+
+	// print as CSV
+	if !noheader {
+		header := strings.Join(columns, ",")
+		fmt.Printf("%s\n", header)
+	}
+	for _, key := range keys {
+		ts := torrentsStatus[key]
+
+		// skip if the name doesn't match the filter
+		if filter != "" && !strings.Contains(strings.ToLower(ts.Name), strings.ToLower(filter)) {
+			continue
+		}
+
+		// format columns and print as CSV
+		var line []string
+		r := rls.ParseString(ts.Name)
 		for _, column := range columns {
-			if !slices.Contains(delugeValidColumns, column) {
-				fmt.Printf("Unknown column: %s\n", column)
-				fmt.Printf("Valid values for --column: %s\n", strings.Join(delugeValidColumns, ", "))
-				os.Exit(1)
-			}
+			line = append(line, delugeFormatColumn(column, ts, r))
 		}
-
-		// create a deluge client
-		if verbosity > 0 {
-			fmt.Printf("server: %s\n", viper.GetString("deluge.server"))
-			fmt.Printf("port: %d\n", viper.GetUint("deluge.port"))
-			fmt.Printf("username: %s\n", viper.GetString("deluge.username"))
-			fmt.Printf("password: %s\n", viper.GetString("deluge.password"))
-		}
-		client := deluge.NewV2(deluge.Settings{
-			Hostname:             viper.GetString("deluge.server"),
-			Port:                 viper.GetUint("deluge.port"),
-			Login:                viper.GetString("deluge.username"),
-			Password:             viper.GetString("deluge.password"),
-			DebugServerResponses: true,
-		})
-
-		// connect
-		err := client.Connect(context.Background())
-		if err != nil {
-			fmt.Printf("Error connecting to deluge: %s\n", err)
-			os.Exit(1)
-		}
-		if verbosity > 0 {
-			fmt.Printf("Connected to deluge\n")
-		}
-
-		// get torrents
-		torrentsStatus, err := client.TorrentsStatus(context.Background(), deluge.StateUnspecified, nil)
-		if err != nil {
-			fmt.Printf("Error getting torrents status: %s\n", err)
-			os.Exit(1)
-		}
-		if verbosity > 0 {
-			fmt.Printf("Found %d torrents\n", len(torrentsStatus))
-		}
-
-		// sort torrentsStatus by name
-		keys := make([]string, 0, len(torrentsStatus))
-		for key, _ := range torrentsStatus {
-			keys = append(keys, key)
-		}
-		sort.Slice(keys, func(i, j int) bool {
-			return torrentsStatus[keys[i]].Name < torrentsStatus[keys[j]].Name
-		})
-
-		// print as CSV
-		if !noheader {
-			header := strings.Join(columns, ",")
-			fmt.Printf("%s\n", header)
-		}
-		for _, key := range keys {
-			ts := torrentsStatus[key]
-
-			// skip if the name doesn't match the filter
-			if filter != "" && !strings.Contains(strings.ToLower(ts.Name), strings.ToLower(filter)) {
-				continue
-			}
-
-			// format columns and print as CSV
-			var line []string
-			r := rls.ParseString(ts.Name)
-			for _, column := range columns {
-				line = append(line, delugeFormatColumn(column, ts, r))
-			}
-			fmt.Printf("%s\n", strings.Join(line, ","))
-		}
-	},
+		fmt.Printf("%s\n", strings.Join(line, ","))
+	}
 }
 
 // format the given column
